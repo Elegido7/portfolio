212a9a3134cc7d4915addad3a6c61c2c0e1400fc
Dennis <dennojunior@gmail.com>
2024-10-31 11:05:27 +0300

    final push for the project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,71 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Portfolio</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus alias
-        ratione fuga repudiandae non pariatur cum distinctio. Quia laudantium deserunt,
-        exercitationem adipisci atque voluptates nihil, quae recusandae saepe dolorem
-        dolores ipsa, libero voluptatem corporis possimus at provident ut blanditiis quam
-        cum. Consequatur odit molestiae inventore rem. Odio earum quae hic perferendis.
-        Quam iste officiis distinctio nemo error. Sed dolor nemo quis doloribus
-        repellendus temporibus architecto aut, aperiam hic ut dolorum, qui ad, incidunt
-        cumque. Facilis tempora, natus perspiciatis fuga odit modi, beatae, ad id labore
-        ducimus aliquid nisi minima rem! Quo dolor nam perferendis saepe dolores vel sunt
-        sit, consectetur, rerum suscipit, laudantium ratione tempora et vitae optio
-        adipisci aliquam? Officiis explicabo alias ut provident quaerat nobis facere esse
-        consequuntur culpa nisi? Vel laudantium ea quod dolorem! Nemo impedit architecto
-        animi consequuntur accusamus, modi illo ducimus rerum harum molestias veniam velit
-        corporis laboriosam enim corrupti accusantium officiis adipisci rem quis.
-      </p>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'Dennis Mburugu',
+        bio: 'I am a frontend Software Developer from Nairobi, Kenya',
+        email: 'dennojunior@gmail.com',
+        institution: 'GoMyCode Kenya',
+        imgSrc: process.env.PUBLIC_URL + '/Dennis.jpg',
+        profession: 'Web Developer',
+      },
+      shows: false,
+      timeSinceMounted: 0,
+    };
+  }
+
+  toggleProfile = () => {
+    this.setState((prevState) => ({
+      shows: !prevState.shows,
+    }));
+  };
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState((prevState) => ({
+        timeSinceMounted: prevState.timeSinceMounted + 1,
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.toggleProfile}>
+          {this.state.shows ? 'Hide Profile' : 'Show Profile'}
+        </button>
+        {this.state.shows && (
+          <div id='profile'>
+            <img src={this.state.person.imgSrc} alt={this.state.person.fullName} />
+            <h2>
+              <span>Name:</span> {this.state.person.fullName}
+            </h2>
+            <p>
+              <span>Email:</span> {this.state.person.email}
+            </p>
+            <p>
+              <span>Bio:</span> {this.state.person.bio}
+            </p>
+            <p>
+              <span>Profession:</span> {this.state.person.profession}
+            </p>
+            <p>
+              <span>Institution:</span> {this.state.person.institution}
+            </p>
+          </div>
+        )}
+        <p id='timer'> <span>Time since component mounted:</span> {this.state.timeSinceMounted} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
