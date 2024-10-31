61403541c490524cdccb699b068af016e02ba8c7
Dennis <dennojunior@gmail.com>
2024-10-31 11:01:57 +0300

    initial portfolio

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Portfolio</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus alias
+        ratione fuga repudiandae non pariatur cum distinctio. Quia laudantium deserunt,
+        exercitationem adipisci atque voluptates nihil, quae recusandae saepe dolorem
+        dolores ipsa, libero voluptatem corporis possimus at provident ut blanditiis quam
+        cum. Consequatur odit molestiae inventore rem. Odio earum quae hic perferendis.
+        Quam iste officiis distinctio nemo error. Sed dolor nemo quis doloribus
+        repellendus temporibus architecto aut, aperiam hic ut dolorum, qui ad, incidunt
+        cumque. Facilis tempora, natus perspiciatis fuga odit modi, beatae, ad id labore
+        ducimus aliquid nisi minima rem! Quo dolor nam perferendis saepe dolores vel sunt
+        sit, consectetur, rerum suscipit, laudantium ratione tempora et vitae optio
+        adipisci aliquam? Officiis explicabo alias ut provident quaerat nobis facere esse
+        consequuntur culpa nisi? Vel laudantium ea quod dolorem! Nemo impedit architecto
+        animi consequuntur accusamus, modi illo ducimus rerum harum molestias veniam velit
+        corporis laboriosam enim corrupti accusantium officiis adipisci rem quis.
+      </p>
+    </div>
+  );
+}
+
+export default App;
